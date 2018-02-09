d501a9969232d29c03b4c7159ed2dedd2fd39381
francaxxx <francasballo@gmail.com>
2018-02-09 02:54:39 +0100

    menu - complete

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+
+  /*Hide menu by default*/
+$("#menu").hide();
+
+/*When menu button is clicked, toggle the menu*/
+$("#menu-btn").click(function(){
+  $("#menu").slideToggle();
+});
